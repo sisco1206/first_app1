dc8dc354c6d62b0edb90352c2ac5dc94ed87e345
Francis Nahounou <programmingfnahounou6@gmail.com>
2024-04-02 12:10:47 -0500

    new feature3 branch

---
+++ file3.py
@@ -0,0 +1,6 @@
+def happy_birthday(name):
+    print(f"Happy birthday to you to {name}!")
+    print(f"Hey {name}, how old are you today?")
+    print(f"Today is a great day for {name} and his wife")
+
+happy_birthday("Francis")    
